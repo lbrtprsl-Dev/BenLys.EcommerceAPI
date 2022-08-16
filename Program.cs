be3b1dac8cd88458f3ebc7b7f4c53fee317ded10
aporsuelo <aporsuelo@traxiontech.net>
2022-08-16 23:29:28 +0800

    Add Comment in Program.cs

--- Program.cs
+++ Program.cs
@@ -7,6 +7,8 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// Add Service Here
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
